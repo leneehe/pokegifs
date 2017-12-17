593e8d984b88de9c126ef4cd5ab9f48bb14f0d35
Lena <fatmeow@gmail.com>
2017-12-17 16:30:42 -0500

    Add gif using giphy api response

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -9,10 +9,15 @@ class PokemonController < ApplicationController
     id = body["id"]
     types = body["types"].map {|type| type["type"]["name"]}
 
+    res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV['GIPHY_KEY']}&q=#{name}&rating=pg")
+    body = JSON.parse(res.body)
+    gif_url = body["data"].sample["url"]
+
     render json: {
       id: id,
       name: name,
       types: types,
+      gif: gif_url,
     }
   end
 end
