bcb68fbec011e843585f278b0e34324bd014c9b2
Lena <fatmeow@gmail.com>
2017-12-17 18:17:19 -0500

    Add team endpoint in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :pokemon, only: %i[index show]
   root 'pokemon#index'
+  get 'team', to: 'pokemon#team'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
