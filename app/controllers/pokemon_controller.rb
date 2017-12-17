e34b6d0307d314b26e831e0a899540d3be265805
Lena <fatmeow@gmail.com>
2017-12-17 18:09:04 -0500

    Complete stretch, add team to index page instead that select 6 random pokemons

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,5 +1,6 @@
 class PokemonController < ApplicationController
   def index
+    team
   end
 
   def show
@@ -9,20 +10,56 @@ class PokemonController < ApplicationController
     if body["detail"] == "Not found."
       render json: { message: "Sorry, we don't have that Pokemon."}
     else
-    name = body["name"]
-    id = body["id"]
-    types = body["types"].map {|type| type["type"]["name"]}
+      name = body["name"]
+      id = body["id"]
+      types = body["types"].map {|type| type["type"]["name"]}
 
-    res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV['GIPHY_KEY']}&q=#{name}&rating=pg")
-    body = JSON.parse(res.body)
-    gif_url = body["data"].sample["url"]
-
-    render json: {
-      id: id,
-      name: name,
-      types: types,
-      gif: gif_url,
-    }
+      res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV['GIPHY_KEY']}&q=#{name}&rating=pg")
+      body = JSON.parse(res.body)
+      if body["data"] == []
+        gif_url = nil
+      else
+        gif_url = body["data"].sample["url"]
+      end
+      render json: {
+        id: id,
+        name: name,
+        types: types,
+        gif: gif_url,
+      }
     end
   end
+
+  def team
+    poketeam = {}
+    6.times do
+      poke_id = rand(802)
+      response = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{poke_id}/")
+      body = JSON.parse(response.body)
+      name = body["name"]
+      id = body["id"]
+      types = body["types"].map {|type| type["type"]["name"]}
+
+      res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV['GIPHY_KEY']}&q=#{name}&rating=pg")
+      body = JSON.parse(res.body)
+      if body["data"] == []
+        gif_url = nil
+      else
+        gif_url = body["data"].sample["url"]
+      end
+
+      pokemon = {
+        id: id,
+        name: name,
+        types: types,
+        gif: gif_url,
+      }
+
+      poketeam[name] = pokemon
+    end
+    render json: {
+      team: poketeam
+    }
+  end
+
 end
